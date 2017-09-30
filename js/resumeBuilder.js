69bae02520ede28c2266384cd93c670cef9c550e
PeMajer <majer.petr@gmail.com>
2017-09-30 20:21:21 +0200

    Budovani portfolia
    Zapinani casti portfolia

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,24 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+/* var firstName="Petr";
+var age=32;
+ 
+var email = "majer.petr@udacity.com";
+var newemail = email.replace ("udacity","gmail");
+console.log(email);
+console.log(newemail); 
+
+var awsomeThoughts="I am Petr and I am Awsome!";
+var funThoughts = awsomeThoughts.replace("Awsome","fun");
+console.log(awsomeThoughts);
+console.log(funThoughts);  */
+
+var formattedname = HTMLheaderName.replace("%data%","Petr Majer");
+$("#header").append (formattedname);
+
+var formattedrole = HTMLheaderRole.replace("%data%","Front-End Sračka");
+$("#header").append (formattedrole);
+
+
+
