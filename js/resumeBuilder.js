4321966e2d2b978c4c6a371eb3e411ae803c0b41
PeMajer <majer.petr@gmail.com>
2017-09-30 21:52:09 +0200

    hrani si s poli

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -15,10 +15,36 @@ console.log(awsomeThoughts);
 console.log(funThoughts);  */
 
 var formattedname = HTMLheaderName.replace("%data%","Petr Majer");
-$("#header").append (formattedname);
-
 var formattedrole = HTMLheaderRole.replace("%data%","Front-End Sračka");
+
+$("#header").append (formattedname);
 $("#header").append (formattedrole);
+
+/*var copiedText = "Petr Majer".slice(5);
+console.log(copiedText);*/
+
+/*var skills = ["Awsomness","PC","Teaching","JS" ];
+$("#main").append (skills);*/
+
+
+// Křestní jméno bylo Albert a příjmení EINSTEIN
+var name = "AlbERt EINstEiN";
+
+function nameChanger(oldName) {
+    var finalName = oldName;
+    jmeno = oldName.split(" ");
+    jmeno[1] = jmeno[1].toUpperCase();
+    jmeno[0] = jmeno[0].slice(0,1) + jmeno[0].slice(1).toLowerCase();
+    finalName = jmeno.join(" ");
+    return finalName;
+};
+
+console.log(nameChanger(name));
+
+
+
+
+
 
 
 
