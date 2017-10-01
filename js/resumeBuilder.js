211394b655524a1e9ba822ea4405ee7ebb624f40
PeMajer <majer.petr@gmail.com>
2017-10-01 14:24:21 +0200

    Objekty
    prace s objekty

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,31 +3,32 @@ This is empty on purpose! Your code to build the resume will go here.
  */
 /* var firstName="Petr";
 var age=32;
- 
+ //-------------------------------------------------
 var email = "majer.petr@udacity.com";
 var newemail = email.replace ("udacity","gmail");
 console.log(email);
 console.log(newemail); 
-
+//-------------------------------------------------
 var awsomeThoughts="I am Petr and I am Awsome!";
 var funThoughts = awsomeThoughts.replace("Awsome","fun");
 console.log(awsomeThoughts);
 console.log(funThoughts);  */
+//-------------------------------------------------
+//var formattedname = HTMLheaderName.replace("%data%","Petr Majer");
+//var formattedrole = HTMLheaderRole.replace("%data%","Front-End Sračka");
 
-var formattedname = HTMLheaderName.replace("%data%","Petr Majer");
-var formattedrole = HTMLheaderRole.replace("%data%","Front-End Sračka");
-
-$("#header").append (formattedname);
-$("#header").append (formattedrole);
-
+//$("#header").append (formattedname);
+//$("#header").append (formattedrole);
+//-------------------------------------------------
 /*var copiedText = "Petr Majer".slice(5);
 console.log(copiedText);*/
+//-------------------------------------------------
+//var skills = ["Awsomness","PC","Teaching","JS" ];
+// $("#main").append (skills);
 
-/*var skills = ["Awsomness","PC","Teaching","JS" ];
-$("#main").append (skills);*/
-
-
+//-------------------------------------------------
 // Křestní jméno bylo Albert a příjmení EINSTEIN
+/*
 var name = "AlbERt EINstEiN";
 
 function nameChanger(oldName) {
@@ -38,13 +39,42 @@ function nameChanger(oldName) {
     finalName = jmeno.join(" ");
     return finalName;
 };
+console.log(nameChanger(name));  
+*/
 
-console.log(nameChanger(name));
+//-------------------------------------------------
+/*var skills = ["Awsomness","PC","Teaching","JS" ];
 
+var bio = {
+	"name" : "Petr Majer",
+	"role" : "JS shit",
+	"skills" : skills,
+	"contact" : {
+		"tel" : "725 279 379",
+		"email" : "majer.p@gmail.com",
+		"location" : "Jihlava"
+	},
+	"pictureURL" : "images/fry.jpg",
+	"welcome": "Vitejte na mem portfoliu"
 
+};
 
+$("#main").append(bio.name); */
+//-------------------------------------------------
 
+var work = {
+	"job" : "teacher",
+	"employer" : 40,
+	"year"	: "2015",
+	"city" : "Jihlava"
+};
 
+var edu = {
+	["schoolName"] : "TU Liberec FP",
+	["start"] : "2000",
+	["end"] : "2010"
 
+};
 
-
+$("#main").append(work["job"]); 
+$("#main").append(edu.schoolName); 
